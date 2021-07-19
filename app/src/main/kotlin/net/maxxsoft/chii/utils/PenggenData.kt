e3631033e8b99f17e90ed433a714c8d55e1ee625
MaxXSoft <x@MaxXSoft.net>
2021-07-19 14:32:17 +0800

    (PenggenData) added data structure used by 'penggen.json'

---
+++ app/src/main/kotlin/net/maxxsoft/chii/utils/PenggenData.kt
@@ -0,0 +1,15 @@
+package net.maxxsoft.chii.utils
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PenggenData(
+    val prob: Double,
+    val txts: List<PenggenTextData>,
+) {
+  @Serializable
+  data class PenggenTextData(
+      val prob: Double,
+      val txt: List<String>,
+  )
+}
