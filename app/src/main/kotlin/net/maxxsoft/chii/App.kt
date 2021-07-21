987d0621e608a7f13f38bb6229b2172149a27029
MaxXSoft <x@MaxXSoft.net>
2021-07-21 11:00:03 +0800

    (App) supported 'shutdown' command

--- app/src/main/kotlin/net/maxxsoft/chii/App.kt
+++ app/src/main/kotlin/net/maxxsoft/chii/App.kt
@@ -1,10 +1,14 @@
 package net.maxxsoft.chii
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+import net.mamoe.mirai.Bot
 import net.mamoe.mirai.BotFactory
 import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.maxxsoft.chii.handlers.AtCommandHandler
 import net.maxxsoft.chii.handlers.MessageHandler
 import net.maxxsoft.chii.utils.Config
 
@@ -14,12 +18,19 @@ private interface App {
 }
 
 private object Main : App {
+  // all bot instances
+  private val bots = ArrayList<Bot>()
+
+  // all coroutines
+  private val jobs = ArrayList<Job>()
+
   override fun run() =
       runBlocking<Unit> {
         // update start time
         Config.updateStartTime()
         // create bot & login
         val bot = BotFactory.newBot(Config.account, Config.password) { fileBasedDeviceInfo() }
+        bots.add(bot)
         bot.login()
         // listen on group message event
         bot.eventChannel.subscribeAlways<GroupMessageEvent> {
@@ -30,12 +41,25 @@ private object Main : App {
           }
         }
         // poll logs
-        launch(Dispatchers.IO) { MessageHandler.pollLogger() }
+        jobs.add(launch(Dispatchers.IO) { MessageHandler.pollLogger() })
+        // detect shutdown command
+        jobs.add(launch { detectShutdown() })
       }
 
   override fun quit() {
+    // cancel all coroutines
+    jobs.forEach { it.cancel() }
     // close logger channel
     MessageHandler.closeLoggerChannel()
+    // close bot
+    bots.forEach { it.close() }
+  }
+
+  private suspend fun detectShutdown() {
+    while (!AtCommandHandler.shutdown) {
+      yield()
+    }
+    quit()
   }
 }
 
